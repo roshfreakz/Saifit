0769ccd344e07ad641699dc8a762126188151ac8
roshfreakz <roshabinash66@gmail.com>
2020-10-20 23:19:23 +0530

    20102020

---
+++ old/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>SAIFIT</title>
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <div>
+        <div class="startTitle">
+            <h3 class="text-primary mb-0">SAIFIT</h3>
+            <p class="text-white">Your Sadhana Tracker</p>
+        </div>
+        <img class="saiimg" src="img/sai.png" alt="sai">
+        <a class="btn btn-light btn-lg btn_start" href="login.php">Get Started</a>
+    </div>
+</body>
+
+</html>
